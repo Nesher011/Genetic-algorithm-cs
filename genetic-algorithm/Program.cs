8a9f9bcb07c234f069b1f9329a3c90d3069eb1cd
Jedrzej Klyza <neshernos@gmail.com>
2022-01-29 22:09:04 +0100

    Dodaj pliki projektów.

---
+++ genetic-algorithm/Program.cs
@@ -0,0 +1,11 @@
+﻿using genetic_algorithm;
+
+Individual individual = new Individual(96);
+
+Console.WriteLine(individual);
+individual.genesList.Add(true);
+//xd.Genes.Add(true);
+ 
+Console.WriteLine(individual.genesList[0]);
+Console.WriteLine(individual.numberOfGenes);
+
---
+++ genetic-algorithm/individual.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace genetic_algorithm
+{
+    public class Individual
+    {
+        public Individual(int numOfGenes)
+        {
+            genesList = new List<bool>();
+            numberOfGenes = numOfGenes;
+        }
+
+        public List<bool> genesList { get; set; }
+
+        public int numberOfGenes { get; set; }
+    }
+}

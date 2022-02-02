98891981104b7c6f13f957d4d24298aa76af449d
Jedrzej Klyza <neshernos@gmail.com>
2022-02-02 23:46:39 +0100

    Generating random bytes for individual, created water pump class

--- genetic-algorithm/Program.cs
+++ genetic-algorithm/Program.cs
@@ -1,11 +1,25 @@
 ﻿using genetic_algorithm;
+Random random = new Random();
+List<Individual> population = new List<Individual>();
+Generation generation=new Generation();
+Water_Pump_Station waterPumpStation=new Water_Pump_Station();
 
-Individual individual = new Individual(96);
+generation.numberOfIndividuals = 40;
+for (int i = 0; i < generation.numberOfIndividuals; i++)
+{    
+    Individual individual = new Individual(96);
+    for (int j = 0; j < individual.numberOfGenes; j++)
+    {
+        individual.genesList.Add(random.NextDouble() >= 0.5);
+    }
+    population.Add(individual);
+}
+waterPumpStation.waterDemand.ForEach(Console.WriteLine);
+waterPumpStation.waterPumpVolume.ForEach(Console.WriteLine);
+waterPumpStation.waterPumpElectricity.ForEach(Console.WriteLine);
 
-Console.WriteLine(individual);
-individual.genesList.Add(true);
-//xd.Genes.Add(true);
- 
-Console.WriteLine(individual.genesList[0]);
-Console.WriteLine(individual.numberOfGenes);
+foreach (Individual x in population)
+{
+    //x.genesList.ForEach(Console.WriteLine);
+};
 
---
+++ genetic-algorithm/Water Pump Station.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace genetic_algorithm
+{
+    internal class Water_Pump_Station
+    {
+        public Water_Pump_Station()
+        {
+            waterDemand = new List<float>();
+            waterPumpVolume = new List<int>();
+            waterPumpElectricity = new List<int>();
+            waterDemand.Add(60);
+            waterDemand.Add(50);
+            waterPumpVolume.Add(0);
+            waterPumpVolume.Add(10);
+            waterPumpElectricity.Add(0);
+            waterPumpElectricity.Add(12);
+        }
+        public List<float> waterDemand { get; }
+        public List<int> waterPumpVolume { get; }
+        public List<int> waterPumpElectricity { get; }
+
+    }
+}
